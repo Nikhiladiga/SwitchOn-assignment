cad2f857edef053b7096faca9237f041942cdcd4
Nikhil Adiga <nikhiladigaz@gmail.com>
2019-11-16 12:48:29 +0530

    Update rootReducer.js

--- client/src/store/reducers/rootReducer.js
+++ client/src/store/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import authReducer from './authReducer';
-import numberReducer from './numberReducer';
 import { combineReducers } from 'redux'
 
 const rootReducer = combineReducers({
     auth: authReducer,
-    number: numberReducer
 });
 
 export default rootReducer;
